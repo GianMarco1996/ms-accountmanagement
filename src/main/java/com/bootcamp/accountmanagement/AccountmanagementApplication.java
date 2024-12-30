23b2ca52c90ff4468b9dd45a37d355d4fefc549d
Gian Castro Morales <jesusgianmarco.castromorales@everis.nttdata.com>
2024-12-29 20:58:20 -0500

    Se registro al servicio de Eureka

--- src/main/java/com/bootcamp/accountmanagement/AccountmanagementApplication.java
+++ src/main/java/com/bootcamp/accountmanagement/AccountmanagementApplication.java
@@ -2,8 +2,10 @@ package com.bootcamp.accountmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountmanagementApplication {
 
 	public static void main(String[] args) {
